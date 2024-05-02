8b75abb6d4108f57eee56f92515ba3ee7aa5e384
SunProwrer <tissartem@gmail.com>
2024-05-02 18:12:19 +0500

    Created backend

---
+++ app/src/main/java/backend/MainActivityBackend.java
@@ -0,0 +1,16 @@
+package backend;
+
+import java.util.List;
+
+public class MainActivityBackend {
+    Room room;
+    List<AbstrctTask> tasks;
+
+    public void onCreate() {
+        loadTasks();
+    }
+
+    public void loadTasks() {
+
+    }
+}
